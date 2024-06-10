fa96a8b2769507ef87d3b09aabf0c967e2db8d7f
zsghhw <zsghhw@icloud.com>
2024-06-10 23:50:10 +0100

    Create MyView.swift

---
+++ Sources/View/MyView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by 张训 on 2024/6/10.
+//
+
+import Foundation
+import SwiftUI
+
+struct MyView: View{
+    var body: some View{
+        Text("Hello World")
+    }
+}
+
+
