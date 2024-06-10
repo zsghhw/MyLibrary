4232e92e174da3ed60d1aa3dd410bc07596e3a15
zsghhw <zsghhw@icloud.com>
2024-06-10 23:59:21 +0100

    Update MyView.swift

--- Sources/View/MyView.swift
+++ Sources/View/MyView.swift
@@ -5,9 +5,10 @@
 //  Created by 张训 on 2024/6/10.
 //
 
-import Foundation
+
 import SwiftUI
 
+@available(ios 13.0, *)
 public struct MyView: View{
     var body: some View{
         Text("Hello World")
